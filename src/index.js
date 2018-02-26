a71d3702d53ab97e92d9434d1653480daf9fc3fc
nox911 <nox.911@gmail.com>
2018-02-26 10:54:00 +0300

    task8

--- src/index.js
+++ src/index.js
@@ -1,43 +1,53 @@
 module.exports = function getZerosCount(number, base) {
-  let zeros_of_base=0;
-  // let zeros_of_two=0;
-  let currentBase = searchBase(base);
-  factorial (number);
-  
-  
-  //calculate whole number of five
-  function five (num) {
-      if (num>=5) {
-          return 	parseInt(num/5)+five(parseInt(num/5));
-      }
-      else {
-          return 0;
-      }
-  }
-  //calculate whole number of two
-  function zerosOfBase (num,curBase) {
-      if (num>=curBase) {
-          return 	parseInt(num/curBase)+zerosOfBase(parseInt(num/curBase),curBase);
-      }
-      else {
-          return 0;
-      }
-  }
+  	let zeros=0;
+  	let zerosBase=0;
+  	let zerosTwo=0;
+  	let doubledBase = 1;
+  	let doubledTwo=0;
+  	let i=2;
+  	let currentBase = searchBase(base);
+  	factorial (number);
 
-  function searchBase (base) {
-      if (base%2===0) {
-          return searchBase(base/2);
-      }
-      else {
-          return base;
-      }
-  }
 
-  function factorial (num) {
-      // zeros_of_five+=five(num);
-      zeros_of_base+=zerosOfBase(num,currentBase);
-  }
+  	function zerosOfBase (num,curBase) {
+      	if (num>=curBase) {
+        	return 	parseInt(num/curBase)+zerosOfBase(parseInt(num/curBase),curBase);
+      	}
+      	else {
+          	return 0;
+      	}
+  	}
 
-  // zeros+=Math.min(zeros_of_five,zeros_of_two);
-  return zeros_of_base;
+  	function searchBase (base) {
+    	if (base<=i+1) {
+        	if (base%2===0) {
+            	doubledTwo++;
+        	}
+      	return base;
+    }
+      	if (base%2===0) {
+         	doubledTwo++;
+          	return searchBase(base/2);
+      	}
+      	else if(base%(i+1)===0) {
+          	doubledBase++;
+        return searchBase(base/(i+1));;
+      	}
+      	else {
+        	i++;
+        	doubledBase=1;
+        	return searchBase(base);
+      	}
+  	}
+
+  	function factorial (num) {
+      	zerosBase+=zerosOfBase(num,currentBase);
+      	zerosTwo+=zerosOfBase(num,2);
+	}
+	  
+	if (doubledTwo===0) {
+    	doubledTwo++;
+	}
+  	zeros=Math.min(parseInt(zerosBase/doubledBase,10),parseInt(zerosTwo/doubledTwo,10));
+  	return zeros;
 }
